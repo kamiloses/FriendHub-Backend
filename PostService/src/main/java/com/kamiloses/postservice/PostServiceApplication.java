0f2ba3677e3c8d75eab56bfb44a3e8e4016874db
kamil <kamil.kurzaj04@gmail.com>
2024-11-15 04:07:23 +0100

    Added Eureka Server and added routing to every module in api gateway

--- PostService/src/main/java/com/kamiloses/postservice/PostServiceApplication.java
+++ PostService/src/main/java/com/kamiloses/postservice/PostServiceApplication.java
@@ -3,10 +3,12 @@ package com.kamiloses.postservice;
 import com.kamiloses.rabbitmq.RabbitConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import(RabbitConfig.class)
+@EnableDiscoveryClient
 public class PostServiceApplication {
 
     public static void main(String[] args) {
