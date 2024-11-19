062c3eb1e39123f0d8a7ee3aeb07ee359bf8a2aa
kamil <kamil.kurzaj04@gmail.com>
2024-11-19 01:37:53 +0100

    application before jwt implementation

--- FriendService/src/main/java/com/kamiloses/friendservice/controller/FriendshipController.java
+++ FriendService/src/main/java/com/kamiloses/friendservice/controller/FriendshipController.java
@@ -2,14 +2,12 @@ package com.kamiloses.friendservice.controller;
 
 import com.kamiloses.friendservice.dto.UserDetailsDto;
 import com.kamiloses.friendservice.service.FriendshipService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/api/friends")
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 public class FriendshipController {
 
 private final FriendshipService friendshipService;
