e9ae4e106bcbb686586b3c25a402bf021c7c864a
kamil <kamil.kurzaj04@gmail.com>
2024-11-24 05:02:20 +0100

    added crossOrigin

--- MessageService/src/main/java/com/kamiloses/messageservice/controller/MessageController.java
+++ MessageService/src/main/java/com/kamiloses/messageservice/controller/MessageController.java
@@ -2,10 +2,7 @@ package com.kamiloses.messageservice.controller;
 
 import com.kamiloses.messageservice.dto.MessageDto;
 import com.kamiloses.messageservice.service.MessageService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -13,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/message")
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 public class MessageController {
 
 private final MessageService messageService;
