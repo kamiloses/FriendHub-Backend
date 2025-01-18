6b3e88f92243bdac641404e0a0e36921fc7afc45
kamil <kamil.kurzaj04@gmail.com>
2025-01-18 09:08:08 +0100

    added opportunity to remove redis values after 24h

--- HashtagService/src/main/java/com/kamiloses/hashtagservice/service/HashtagService.java
+++ HashtagService/src/main/java/com/kamiloses/hashtagservice/service/HashtagService.java
@@ -1,27 +1,29 @@
 package com.kamiloses.hashtagservice.service;
 
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
-@Service
-public class HashtagService {
-
-
-
- public List<String> getMostPopularHashtags() {
-
-
-
-
-
- }
-
-
-
+//@Service
+//public class HashtagService {
+//
+//    private RedisTemplate<String, String> redisTemplate;
+//
+// public List<String> getMostPopularHashtags() {
+//     Map<String,String> allHashtags = redisTemplate.opsForHash().;
+//
+//
+//
+//
+// }
 
 
 
 
 
-}
+
+
+
+
