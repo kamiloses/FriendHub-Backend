fada8ca4459e0aac0be602766920895578d2a283
kamil <kamil.kurzaj04@gmail.com>
2025-02-18 08:14:33 +0100

    removed notification module

--- NotificationsService/src/main/java/com/kamiloses/notificationsservice/repository/NotificationsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kamiloses.notificationsservice.repository;
-
-import com.kamiloses.notificationsservice.entity.NotificationsEntity;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface NotificationsRepository extends ReactiveMongoRepository<NotificationsEntity, String> {
-}
